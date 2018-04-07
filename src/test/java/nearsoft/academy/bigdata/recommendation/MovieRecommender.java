203afb8b17e4af6922d1a55b109fb423ed4808e8
ErnestoSalazar <ernestoalbertosalazar@gmail.com>
2018-04-07 10:59:08 -0700

    Added MovieRecommender class to use in Test

---
+++ src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
@@ -0,0 +1,164 @@
+package nearsoft.academy.bigdata.recommendation;
+
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+
+import java.io.*;
+
+import java.util.*;
+import java.util.zip.GZIPInputStream;
+
+public class MovieRecommender {
+
+
+    private BiMap<Integer, String> mapProducts = HashBiMap.create();
+    private BiMap<Integer, String> mapUsers = HashBiMap.create();
+
+    List<String> usersIdArray = new ArrayList<>();
+    List<String> productsIdArray = new ArrayList<>();
+    List<Double> reviewsArray = new ArrayList<>();
+
+    private File fileInput;
+    private FileWriter fileWriter;
+
+    public MovieRecommender(String path) throws IOException {
+        this.fileWriter = new FileWriter("movies.csv");
+        this.readInputFile(path);
+    }
+
+    public int getTotalReviews(){
+        return this.reviewsArray.size();
+    }
+
+    public int getTotalProducts(){
+        return this.mapProducts.size();
+    }
+
+    public int getTotalUsers(){
+        return this.mapUsers.size();
+    }
+
+
+
+    private void readInputFile(String path) throws IOException {
+
+
+        String PRODUCT_ID ="product/productId: ";
+        String USER_ID = "review/userId: ";
+        String SCORE = "review/score: ";
+
+        String readedLine;
+
+        InputStream fileStream = null;
+        fileStream = new FileInputStream(path);
+        InputStream gzipStream = new GZIPInputStream(fileStream);
+        Reader decoder = new InputStreamReader(gzipStream, "UTF-8");
+        BufferedReader bufferedReader = new BufferedReader(decoder);
+
+        int mapUserKey = 0;
+        int mapProductKey = 0;
+
+        while((readedLine = bufferedReader.readLine()) != null){
+            if(readedLine.contains(PRODUCT_ID)){
+                String splitedLine = readedLine.split(PRODUCT_ID)[1];
+                this.productsIdArray.add(splitedLine);
+                if(!this.mapProducts.containsValue(splitedLine)){
+                    this.mapProducts.put(mapProductKey, splitedLine);
+                    mapProductKey++;
+                }
+            }else if(readedLine.contains(USER_ID)){
+                String splitedLine = readedLine.split(USER_ID)[1];
+                this.usersIdArray.add(splitedLine);
+                if(!this.mapUsers.containsValue(splitedLine)){
+                    this.mapUsers.put(mapUserKey, splitedLine);
+                    mapUserKey++;
+                }
+            }else if(readedLine.contains(SCORE)){
+                String splitedLine = readedLine.split(SCORE)[1];
+                this.reviewsArray.add(Double.parseDouble(splitedLine));
+            }
+        }
+        this.writeCSV();
+        System.out.println("csv created");
+        System.out.println(this.mapProducts.size());
+        System.out.println(this.mapUsers.size());
+        System.out.println(this.reviewsArray.size());
+        this.closeCSV();
+    }
+
+    public BiMap<Integer, String> getMap(BiMap<Integer, String> biMap, String data){
+        int id = 1;
+        if(!biMap.containsKey(data)){
+            biMap.put(id,data);
+            id++;
+        }
+        return biMap;
+    }
+
+
+
+    private void writeCSV(){
+        try {
+            for (int i = 0; i < this.usersIdArray.size()-1; i++) {
+                Integer idUser = this.mapUsers.inverse().get(this.usersIdArray.get(i));
+                this.fileWriter.append(String.valueOf(idUser));
+                this.fileWriter.append(",");
+
+                Integer idProduct = this.mapProducts.inverse().get(this.productsIdArray.get(i));
+                this.fileWriter.append(String.valueOf(idProduct));
+                this.fileWriter.append(",");
+
+                this.fileWriter.append(String.valueOf(this.reviewsArray.get(i)));
+
+                this.fileWriter.append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeCSV(){
+        try{
+            this.fileWriter.flush();
+            this.fileWriter.close();
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public List<String> getRecommendationsForUser(String userId){
+        List<String> listRecommendations = new ArrayList<>();
+        try {
+
+            List<RecommendedItem> recommendations = null;
+
+            DataModel model = new FileDataModel(new File("movies.csv"));
+            UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+            UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
+            UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+            recommendations = recommender.recommend(this.mapUsers.inverse().get(userId), 3);
+            for (RecommendedItem recommendation : recommendations) {
+                listRecommendations.add(this.mapProducts.get((int) recommendation.getItemID()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TasteException e) {
+            e.printStackTrace();
+        }
+        return listRecommendations;
+    }
+
+
+}
